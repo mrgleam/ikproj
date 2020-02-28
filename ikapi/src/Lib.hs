9fde622eef86a09c7eb8eb68739d504a3e8d1ff5
Santi <santi@odds.team>
2020-02-28 18:39:49 +0700

    ✨ NEW: add servant auth and make authentication

--- ikapi/src/Lib.hs
+++ ikapi/src/Lib.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE DeriveGeneric     #-}
+
 module Lib
-    ( startApp
-    , app
-    ) where
+    ( startApp) where
 
 import Data.Aeson
 import Data.Aeson.TH
@@ -12,32 +14,70 @@ import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.Wai.Logger ( withStdoutLogger )
 import Servant
+import Servant.Auth.Server
+import Control.Monad.IO.Class ( liftIO )
+import GHC.Generics ( Generic )
+
+data Login = Login { username :: String, password :: String }
+        deriving (Eq, Show, Read, Generic)
+
+$(deriveJSON defaultOptions ''Login)
+
+checkCreds :: CookieSettings -> JWTSettings -> Login
+  -> Handler (Headers '[ Header "Set-Cookie" SetCookie
+                       , Header "Set-Cookie" SetCookie]
+                       NoContent)
+checkCreds cookieSettings jwtSettings (Login "Ali Baba" "Open Sesame") = do
+   let usr = User 1 "Ali Baba" "ali@email.com"
+   mcookie <- liftIO $ acceptLogin cookieSettings jwtSettings usr
+   case mcookie of
+     Nothing     -> throwError err401
+     Just applyCookies -> return $ applyCookies NoContent
+checkCreds _ _ _ = throwError err401
+
+type Protected = "name" :> Get '[JSON] String
+                :<|> "email" :> Get '[JSON] String
+
+type Unprotected =
+  "login"
+    :> ReqBody '[JSON] Login
+    :> Verb 'POST 204 '[JSON] (Headers '[ Header "Set-Cookie" SetCookie
+                                        , Header "Set-Cookie" SetCookie]
+                                        NoContent) 
+    :<|> Raw
+
+protected :: AuthResult User -> Server Protected
+protected (Authenticated user) =
+  return (userFirstName user) :<|> return (userLastName user)
+protected _ = throwAll err401
+
+unprotected :: CookieSettings -> JWTSettings -> Server Unprotected
+unprotected cs jwts =
+  checkCreds cs jwts :<|> serveDirectoryFileServer "static"
 
 data User = User
   { userId        :: Int
   , userFirstName :: String
   , userLastName  :: String
-  } deriving (Eq, Show)
+  } deriving (Eq, Show, Read, Generic)
+
+instance ToJWT User
+instance FromJWT User
 
 $(deriveJSON defaultOptions ''User)
 
-type API = "users" :> Get '[JSON] [User]
+type API auths = (Auth auths User :> Protected)
+                  :<|> Unprotected
+
+server :: CookieSettings -> JWTSettings -> Server (API auths)
+server cs jwts =
+  protected :<|> unprotected cs jwts
 
 startApp :: IO ()
 startApp = withStdoutLogger $ \aplogger -> do
-        let settings = setPort 8080 $ setLogger aplogger defaultSettings
-        runSettings settings app
-
-app :: Application
-app = serve api server
-
-api :: Proxy API
-api = Proxy
-
-server :: Server API
-server = return users
-
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        ]
+  let settings = setPort 8080 $ setLogger aplogger defaultSettings
+  myKey <- generateKey
+  let jwtCfg = defaultJWTSettings myKey
+      cfg    = defaultCookieSettings :. jwtCfg :. EmptyContext
+      api    = Proxy :: Proxy (API '[JWT])
+  runSettings settings $ serveWithContext api cfg (server defaultCookieSettings jwtCfg)
