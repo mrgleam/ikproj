31e7e4b17b72b50d3ef18090f5341d22a69d3424
Santi <santi@odds.team>
2020-04-27 10:02:45 +0700

    Revert "⚗ EXPERIMENTAL: try to build docker image"
    This reverts commit 18ca2af63faeed31de8df2bc0a0349b228bcc937.

--- ikweb-hs/default.nix
+++ ikweb-hs/default.nix
@@ -1,13 +1,11 @@
 let
 
   pkgs = import ./nixpkgs.nix;
-  ikproj-src = ../. ;
-
   server = pkgs.callPackage ./server/default.nix {};
   client = pkgs.callPackage ./client/default.nix {};
 
 in pkgs.runCommand "ikproj" { inherit client server; } ''
-  mkdir -p $out/{bin,static}
+  mkdir -p $out/bin/static
   cp ${server}/bin/* $out/bin/
-  ${pkgs.closurecompiler}/bin/closure-compiler ${client}/bin/client.jsexe/all.js > $out/static/all.js
+  cp ${client}/static/* $out/bin/static/
 ''
