18ca2af63faeed31de8df2bc0a0349b228bcc937
Santi <santi@odds.team>
2020-04-27 09:51:49 +0700

    ⚗ EXPERIMENTAL: try to build docker image

--- ikweb-hs/default.nix
+++ ikweb-hs/default.nix
@@ -1,11 +1,13 @@
 let
 
   pkgs = import ./nixpkgs.nix;
+  ikproj-src = ../. ;
+
   server = pkgs.callPackage ./server/default.nix {};
   client = pkgs.callPackage ./client/default.nix {};
 
 in pkgs.runCommand "ikproj" { inherit client server; } ''
-  mkdir -p $out/bin/static
+  mkdir -p $out/{bin,static}
   cp ${server}/bin/* $out/bin/
-  cp ${client}/static/* $out/bin/static/
+  ${pkgs.closurecompiler}/bin/closure-compiler ${client}/bin/client.jsexe/all.js > $out/static/all.js
 ''
